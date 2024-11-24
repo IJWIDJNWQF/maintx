ddc7b8b9ce91670c44332057464663fdefa64ced
nospecs24f0 <93601777+IJWIDJNWQF@users.noreply.github.com>
2024-11-24 16:08:05 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,1 @@
-(getgenv()).key = "MARUYG3D6IVRHDPMN75KS27XG";
-(getgenv()).id = "920482498771836969";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+
